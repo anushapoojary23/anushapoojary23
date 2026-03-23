c16baddb0014754c4ca891ff7302b18fde68e2c3
anushapoojary23 <anushapoojary255@gmail.com>
2026-03-23 14:54:46 +0530

    Add files via upload

---
+++ matrix.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.*;
+class matrix
+{
+	public static void main(String args[])
+	{
+		int i,j,row,col,sum;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the row size:");
+		row=sc.nextInt();
+		int a[][]=new int[row][];
+		for(i=0;i<row;i++)
+		{
+			System.out.println("Enter the column size for row"+(i+1));
+			col=sc.nextInt();
+			System.out.println("Enter the row value");
+			a[i]=new int[col];
+			for(j=0;j<col;j++)
+			a[i][j]=sc.nextInt();
+		}
+		System.out.println("Input matrix:");
+		for(int first[]:a)
+		{
+			for(int second:first)
+			{
+				System.out.println(second+"\t");
+			}
+			System.out.println("");
+		}
+		for(int first[]:a)
+		{
+			sum=0;
+			for(int second:first)
+			{
+				sum=sum+second;
+			}
+			System.out.println("the sum of each row is:"+sum);
+		}
+	}
+}
